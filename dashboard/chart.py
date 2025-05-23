bb9075569981c5f2f25279ae586d2321ba2b1df0
Natali <nataliharju@gmail.com>
2025-05-23 11:26:14 +0200

    designing dashboard, added gemini with chunks, and empty page for embedding

--- dashboard/chart.py
+++ dashboard/chart.py
@@ -8,7 +8,7 @@ def bar_chart(df: pd.DataFrame, x: str, y: str, title: str):
         df.sort_values(by=y, ascending=False), 
         x=x, 
         y=y, 
-        title=title,
+        #title=title,
         color=y,
         color_continuous_scale="Teal"               
     )
@@ -18,7 +18,7 @@ def bar_chart(df: pd.DataFrame, x: str, y: str, title: str):
 
 # Creating a pie chart using Plotly Express
 def pie_chart(df: pd.DataFrame, names: str, values: str, title: str):
-    fig = px.pie(df, names=names, values=values, title=title, hole=0.3)
+    fig = px.pie(df, names=names, values=values, hole=0.3) #title=title, )
     return fig
 
 # Show text in Streamlit
@@ -59,17 +59,17 @@ def map_chart(df: pd.DataFrame, title: str = "Antal lediga jobb per stad"):
 
 
 # Detailed_overview: Metod för att få ut barplot, används i kolumn 1, workplace city, employer name, occupation
-def chart_top_occupations(current_df, sort_on):
+def chart_top_occupations(current_df, sort_on, field):
 
     df = current_df.groupby(sort_on)["vacancies"].sum().sort_values(ascending=False).reset_index()
     bar = st.slider("Antal staplar att visa", min_value=10, max_value=20, step=1)
 
-    fig = px.bar(df.head(bar), x=sort_on, y="vacancies")
-    fig.update_layout(title_text=f"Top {bar} {sort_on}s with most vacancies in field: field")
+    fig = px.bar(df.head(bar), x=sort_on, y="vacancies", color="vacancies",color_continuous_scale="Teal" )
+    # fig.update_layout(title_text=f"Topp {bar} {sort_on}s with most vacancies in field: {field}")
     return fig, df
 
 #Detaied_overview: Metod för att få ut pie-chart, används i kolumn 2, languages, duration, working hours type
-def general_pie_chart(current_df, sort_on,get_unique,detailed_sort):
+def general_pie_chart(current_df, sort_on,get_unique, detailed_sort):
 
     df = current_df[current_df[detailed_sort] != 'ej angiven']
     df = df[df[sort_on] == get_unique]
--- dashboard/job_dashboards.py
+++ dashboard/job_dashboards.py
@@ -1,19 +1,20 @@
 import streamlit as st
-from pages_ import home, detailed_overview, gemini
+from pages_ import home, detailed_overview, gemini, gemini_chunks, embedding
 import os 
 import duckdb
 from pathlib import Path
-# from pages import file_1
 
 def layout():
     groups = {
-        "mart_ads": "mart_ads",
-        "mart_installation_drift_underhall": "mart_installation_drift_underhall", #Installation, Drift, Underhåll
-        "mart_kultur_media": "mart_kultur_media",
-        "mart_kropp_skonhet": "mart_kropp_skonhet"
+        "Alla jobb": "mart_ads",
+        "Installation, Drift, Underhåll": "mart_installation_drift_underhall",
+        "Kultur, Media, Design": "mart_kultur_media",
+        "Kropp och skönhetsvård": "mart_kropp_skonhet"
     }
-    group = st.sidebar.radio("Välj yrkesgrupp", list(groups.keys()))
-    working_directory = Path(__file__).parents[1]
+    field = st.sidebar.radio("Välj yrkesgrupp", list(groups.keys()))
+    group = groups[field] #group = "Alla[mart_ads]"
+    
+    working_directory = Path(__file__).parents[1] # Hämta ut DF
     os.chdir(working_directory)
     with duckdb.connect("ads_data.duckdb") as connection:
         df = connection.execute(f"SELECT * FROM mart.{group}").df()
@@ -21,14 +22,14 @@ def layout():
     page = {
         "Home": home.home_layout, 
         "Detailed overview": detailed_overview.overview_layout,
-        "Ask Gemini": gemini.gemini_layout
+        "Ask Gemini": gemini.gemini_layout,
+        "Ask Gemini with chunks and plots": gemini_chunks.gemini_chunks_layout,
+        "Find the best ad - with embedding": embedding.embedding_layout
     }
-    choice = st.sidebar.radio("Välj vy", list(page.keys()))
+    choice_view = st.sidebar.radio("Välj vy", list(page.keys()))
     
-    page[choice](df)
+    page[choice_view](df, field) 
     
-
-
 
 if __name__ == "__main__":
     layout()
