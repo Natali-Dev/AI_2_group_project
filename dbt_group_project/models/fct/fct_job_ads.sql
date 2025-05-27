3779c3ea94750a8a4935853d5b71b02edf454106
Edwin Madu Lindblom <edwmad0323@gmail.com>
2025-05-27 11:12:20 +0200

    Push innan merge till main

--- dbt_group_project/models/fct/fct_job_ads.sql
+++ dbt_group_project/models/fct/fct_job_ads.sql
@@ -1,14 +1,11 @@
-with job_ads as (
-    select * from {{ ref('src_job_ads') }}
-)
+with job_ads as (select * from {{ ref('src_job_ads') }})
 
 select
-    {{ dbt_utils.generate_surrogate_key(['occupation__label']) }} as occupation_id,  -- Använd occupation_field direkt
+    {{ dbt_utils.generate_surrogate_key(['occupationlabel']) }} as occupation_id,
     {{ dbt_utils.generate_surrogate_key(['id']) }} as job_details_id,
-    {{ dbt_utils.generate_surrogate_key(['employer_workplace', 'workplace_city']) }} as employer_id,
+    {{ dbt_utils.generate_surrogate_key(['employerworkplace', 'workplace_address__municipality']) }} as employer_id,
     {{ dbt_utils.generate_surrogate_key(['experience_required','driving_license_required', 'access_to_own_car']) }} as auxilliary_attributes_id,
-    vacancies,
+        coalesce((number_of_vacancies), 1) as vacancies, 
     relevance,
-    application_deadline,
-    occupation_field
+    application_deadline
 from job_ads
