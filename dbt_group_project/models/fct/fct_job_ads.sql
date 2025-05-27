a8a62991848c0e462a4296853dbb67da8b0bf353
Edwin Madu Lindblom <edwmad0323@gmail.com>
2025-05-27 11:48:33 +0200

    Revert " Push innan merge till main"
    This reverts commit 3779c3ea94750a8a4935853d5b71b02edf454106.

--- dbt_group_project/models/fct/fct_job_ads.sql
+++ dbt_group_project/models/fct/fct_job_ads.sql
@@ -1,11 +1,14 @@
-with job_ads as (select * from {{ ref('src_job_ads') }})
+with job_ads as (
+    select * from {{ ref('src_job_ads') }}
+)
 
 select
-    {{ dbt_utils.generate_surrogate_key(['occupationlabel']) }} as occupation_id,
+    {{ dbt_utils.generate_surrogate_key(['occupation__label']) }} as occupation_id,  -- Använd occupation_field direkt
     {{ dbt_utils.generate_surrogate_key(['id']) }} as job_details_id,
-    {{ dbt_utils.generate_surrogate_key(['employerworkplace', 'workplace_address__municipality']) }} as employer_id,
+    {{ dbt_utils.generate_surrogate_key(['employer_workplace', 'workplace_city']) }} as employer_id,
     {{ dbt_utils.generate_surrogate_key(['experience_required','driving_license_required', 'access_to_own_car']) }} as auxilliary_attributes_id,
-        coalesce((number_of_vacancies), 1) as vacancies, 
+    vacancies,
     relevance,
-    application_deadline
+    application_deadline,
+    occupation_field
 from job_ads
