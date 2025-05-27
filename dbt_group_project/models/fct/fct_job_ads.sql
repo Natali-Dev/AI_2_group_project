8e53f2ae50ca144b051f4f59b645f9fa17be423a
Natali <nataliharju@gmail.com>
2025-05-27 14:07:25 +0200

    adding coalesce for vacancies in fct, update readme

--- dbt_group_project/models/fct/fct_job_ads.sql
+++ dbt_group_project/models/fct/fct_job_ads.sql
@@ -5,7 +5,7 @@ select
     {{ dbt_utils.generate_surrogate_key(['id']) }} as job_details_id,
     {{ dbt_utils.generate_surrogate_key(['employer__workplace', 'workplace_address__municipality']) }} as employer_id,
     {{ dbt_utils.generate_surrogate_key(['experience_required','driving_license_required', 'access_to_own_car']) }} as auxilliary_attributes_id,
-    number_of_vacancies as vacancies,
+    coalesce (number_of_vacancies, 1) as vacancies,
     relevance,
     application_deadline
 from job_ads 
